2e61d2771e8942960e68e48b3e20c358f75d0e47
Seungyeon Song <ream123@naver.com>
2020-10-07 02:10:03 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+function Food({favorite}){
+  return <h1>I like {favorite}</h1>;
+}
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      hello!!
+      <Food favorite="kimchi" />
+    </div>);
 }
 
 export default App;
